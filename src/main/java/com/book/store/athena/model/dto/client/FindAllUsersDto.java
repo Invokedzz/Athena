c096e2dee6cdba92fd538a80252f69cd738a43f2
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-03-04 21:09:54 -0300

    Adding the user profile

--- src/main/java/com/book/store/athena/model/dto/client/FindAllUsersDto.java
+++
@@ -1,4 +0,0 @@
-package com.book.store.athena.model.dto.client;
-
-public record FindAllUsersDto () {
-}
--- src/main/java/com/book/store/athena/model/repository/UserRepository.java
+++ src/main/java/com/book/store/athena/model/repository/UserRepository.java
@@ -4,12 +4,15 @@ import com.book.store.athena.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @SuppressWarnings("NullableProblems")
 public interface UserRepository extends JpaRepository <User, Long> {
 
-    Optional<User> findById (Long id);
+    Optional <User> findById (Long id);
+
+    List <User> findUserById (Long id);
 
 }
