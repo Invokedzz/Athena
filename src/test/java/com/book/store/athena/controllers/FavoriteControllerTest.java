e884a3717afaca58042addcfa0981e39eacc033d
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-03-17 11:32:43 -0300

    finishing the unit tests, for now

--- src/test/java/com/book/store/athena/controllers/FavoriteControllerTest.java
+++ src/test/java/com/book/store/athena/controllers/FavoriteControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
